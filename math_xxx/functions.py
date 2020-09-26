5b8f370aa0543b165295fb11c30c7cea4aa6ecf8
alebrd <71253112+alebrd@users.noreply.github.com>
2020-09-26 09:52:32 +0200

    -committing updates-

---
+++ math_xxx/functions.py
@@ -0,0 +1,24 @@
+def multiply(a, b):
+    return a * b
+
+
+def division(a, b):
+    return a / b
+
+
+def addition(a, b):
+    return a + b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+print(subtraction(10, 5))
+
+print(addition(10, 5))
+
+print(division(10, 5))
+
+print(multiply(5, 2))
+
